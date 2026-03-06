9a3c272a01d3df4e20b125569809baaf5139b411
gddhy <gddhywgy@gmail.com>
2026-03-06 13:48:37 +0800

    调整到gcore

--- fs.js
+++ fs.js
@@ -2,7 +2,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
 (function () {
     function runWithFS() {
         //const path = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/')) + '/fs/';
-        const path = 'https://testingcf.jsdelivr.net/gh/gddhy/mrpvm/fs/';
+        const path = 'https://gcore.jsdelivr.net/gh/gddhy/mrpvm/fs/';
 
         const dirs = [
             "/mythroad",
